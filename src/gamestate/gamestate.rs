7a4c756e99083f0e78e81c8bc4ff084acd7cbdb7
brian <brianjsummerfield@gmail.com>
2023-11-22 23:37:27 -0600

    timer and websockets working

--- src/gamestate/gamestate.rs
+++ src/gamestate/gamestate.rs
@@ -1,7 +1,9 @@
+use maud::html;
+use std::collections::HashSet;
 use std::sync::{Arc, Mutex};
-use std::thread;
 use std::time::Duration;
-
+use tokio::sync::broadcast;
+use tokio::time;
 // Define possible game states
 pub enum GameStateEnum {
     Starting,
@@ -9,61 +11,59 @@ pub enum GameStateEnum {
     GameOver,
 }
 
-// GameState struct
+//create a game state struct to hold the game state and the broadcast channel sender for sending messages to the clients (players)
 pub struct GameState {
+    user_set: HashSet<String>,
     timer: u32,
     state: GameStateEnum,
+    pub tx: broadcast::Sender<String>,
 }
 
+// Implement the GameState struct with a new function well want to also create a funciont to
+// increment the timer by 1 every second.  The timer is what we'll be broadcasting to the clients
+
 impl GameState {
-    // Constructor for a new GameState
-    pub fn new() -> Arc<Mutex<Self>> {
-        let game_state = GameState {
+    pub fn new() -> Self {
+        let (tx, _) = broadcast::channel(10);
+        Self {
+            user_set: HashSet::new(),
             timer: 0,
-            state: GameStateEnum::Starting, // initial state
-        };
+            state: GameStateEnum::Starting,
+            tx,
+        }
+    }
 
-        let arc_state = Arc::new(Mutex::new(game_state));
-        let thread_state = Arc::clone(&arc_state);
+    //create a function that increments the timer field everyone second.  We'll spawn this function in the main function  and it will run in the background for the duration of the program
+    //our timer will be broadcasted to the websockets everytime we increment
 
-        // Timer thread
-        thread::spawn(move || loop {
-            thread::sleep(Duration::from_secs(1));
-            let mut state = thread_state.lock().unwrap();
-
-            match state.state {
-                GameStateEnum::Starting => {}
-                GameStateEnum::InProgress => {
-                    state.timer += 1;
-                    println!("Timer: {}", state.timer);
-                }
-                GameStateEnum::GameOver => {
-                    // The timer does not update in this state
+    pub fn start_timer(game_state: Arc<Mutex<Self>>) {
+        tokio::spawn(async move {
+            loop {
+                tokio::time::sleep(Duration::from_secs(1)).await;
+                let mut state = match game_state.lock() {
+                    Ok(guard) => guard,
+                    Err(poisoned) => {
+                        eprintln!("Mutex was poisoned. Using poisoned data.");
+                        poisoned.into_inner()
+                    }
+                };
+                state.timer += 1;
+                let timer_html = state.timer();
+                if let Err(e) = state.tx.send(timer_html) {
+                    eprintln!("Failed to send timer update: {}", e);
                 }
             }
         });
-
-        arc_state
     }
 
-    // Method to update the game state
-    pub fn update_state(&mut self, new_state: GameStateEnum) {
-        self.state = new_state;
-
-        match self.state {
-            GameStateEnum::Starting => {
-                // Actions to perform when the game is starting
-                self.timer = 0; // Example: Reset the timer
-                println!("Game is starting, initializing...");
-            }
-            GameStateEnum::InProgress => {
-                // Start or continue the timer
-                println!("Game in progress...");
-            }
-            GameStateEnum::GameOver => {
-                // Stop the timer and clean up
-                println!("Game over.");
+    //create a function that generates the maud html template that has a div with the id of "timer"
+    //and the contents are the timer value
+    pub fn timer(&self) -> String {
+        html! {
+            div id="game_timer" {
+                (self.timer)
             }
         }
+        .into_string()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,47 @@
-use axum::{response::Html, routing::get, Extension, Router};
+use axum::extract::{
+    ws::{Message, WebSocket, WebSocketUpgrade},
+    State,
+};
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Extension, Router,
+};
 use maud::html;
 use std::net::SocketAddr;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use tower_http::services::ServeDir;
+
+use futures::{sink::SinkExt, stream::StreamExt};
 
 mod boggle;
 use boggle::BoggleBoard;
 
 mod dictionary;
 use dictionary::Dictionary;
-
 mod gamestate;
-use gamestate::{GameState, GameStateEnum};
+use gamestate::GameState;
 
 #[tokio::main]
 async fn main() {
     // Set up the router
-    let game_state = GameState::new();
 
-    game_state
-        .lock()
-        .unwrap()
-        .update_state(GameStateEnum::InProgress);
+    let game_state = Arc::new(Mutex::new(GameState::new()));
+    GameState::start_timer(game_state.clone());
 
     let file_path = format!(
         "{}/static/scrabble-dictionary.txt",
         env!("CARGO_MANIFEST_DIR")
     );
     let dictionary = Arc::new(Dictionary::new(&file_path).expect("Failed to create dictionary"));
+
     let app = Router::new()
         .route("/", get(serve_boggle_board))
         .layer(Extension(dictionary))
         // Serve static files from the `static` directory
-        .nest_service("/static", ServeDir::new("static"));
+        .nest_service("/static", ServeDir::new("static"))
+        .route("/ws", get(websocket_handler))
+        .with_state(game_state);
 
     // Bind to a socket address
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -45,6 +54,29 @@ async fn main() {
         .unwrap();
 }
 
+async fn websocket_handler(
+    ws: WebSocketUpgrade,
+    State(state): State<Arc<Mutex<GameState>>>,
+) -> impl IntoResponse {
+    ws.on_upgrade(|socket| websocket(socket, state))
+}
+// create a websocket function for handling sending websocket messages to the clients
+async fn websocket(ws: WebSocket, state: Arc<Mutex<GameState>>) {
+    let (mut tx, _) = ws.split();
+    // Subscribe to the broadcast channel
+    let mut rx = {
+        let state_locked = state.lock().unwrap_or_else(|e| e.into_inner());
+        state_locked.tx.subscribe()
+    };
+    // Loop over messages received on the broadcast channel
+    while let Ok(msg) = rx.recv().await {
+        // Send the message to the client
+        if tx.send(Message::Text(msg)).await.is_err() {
+            break;
+        }
+    }
+}
+
 async fn serve_boggle_board(Extension(dictionary): Extension<Arc<Dictionary>>) -> Html<String> {
     let boggle_board = BoggleBoard::new(&dictionary);
     let markup = html! {
@@ -52,11 +84,17 @@ async fn serve_boggle_board(Extension(dictionary): Extension<Arc<Dictionary>>) -
         html {
             head {
                 title { "Boggle Game" }
-                // Include your stylesheet and scripts here
-                link rel="stylesheet" href="/static/style.css";
+                script
+                    src="https://unpkg.com/htmx.org@1.9.9"
+                    integrity="sha384-QFjmbokDn2DjBjq+fM+8LUIVrAgqcNW2s0PjAxHETgRn9l4fvX31ZxDxvwQnyMOX"
+                    crossorigin="anonymous" {}
+                script src="https://unpkg.com/htmx.org/dist/ext/ws.js" {}
+               link rel="stylesheet" href="/static/style.css";
             }
             body {
                 h1 { "Boggle Game" }
+                div hx-ext="ws" ws-connect="/ws" {}
+                div id="game_timer" {}
                 div id="game-board" {
                     // Render the Boggle board
                     @for row in &boggle_board.board {
@@ -87,8 +125,3 @@ async fn serve_boggle_board(Extension(dictionary): Extension<Arc<Dictionary>>) -
 
     Html(markup.into_string())
 }
-
-// async fn serve_websocket() -> Html<String> {
-//     // Placeholder for the WebSocket handling
-//     Html("WebSocket setup will go here.".to_string())
-// }
