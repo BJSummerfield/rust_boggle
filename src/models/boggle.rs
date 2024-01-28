2fea01949035627559ae5d90dcba016e36370c44
Brian <brian@MacBook-Pro-2.local>
2024-01-28 17:07:02 -0500

    timer added as a model

--- src/models/boggle.rs
+++ src/models/boggle.rs
@@ -1,9 +1,9 @@
-use crate::models::{Board, Dictionary, PlayerId, PlayerList};
+use crate::models::{Board, Dictionary, PlayerId, PlayerList, Timer};
 use crate::render::Render;
 
 use maud::html;
-use std::{env, sync::Arc, time::Duration};
-use tokio::sync::{broadcast, Mutex, Notify};
+use std::{env, sync::Arc};
+use tokio::sync::{broadcast, Mutex};
 
 // Define possible game states
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -20,13 +20,14 @@ pub struct Boggle {
     state: BoggleStateEnum,
     board: Board,
     dictionary: Arc<Dictionary>,
-    timer: u32,
-    timer_cancel_token: Arc<Notify>,
+    timer: Arc<Mutex<Timer>>,
     pub tx: broadcast::Sender<String>,
     boggle_channel_tx: broadcast::Sender<BoggleStateEnum>,
 }
 
 impl Boggle {
+    pub const GAME_DURATION: u32 = 180;
+
     pub fn new() -> Arc<Mutex<Self>> {
         let styles_path =
             env::var("STATIC_FILES_PATH").unwrap_or_else(|_| "/app/static".to_string());
@@ -36,19 +37,19 @@ impl Boggle {
 
         let (tx, _) = broadcast::channel(10);
         let (boggle_channel_tx, _) = broadcast::channel(1);
-        let timer_cancel_token = Arc::new(Notify::new());
+        let timer = Timer::new(tx.clone(), boggle_channel_tx.clone());
         let boggle = Arc::new(Mutex::new(Self {
             players: PlayerList::new(),
             board: Board::new(&dictionary),
             dictionary,
             boggle_channel_tx,
             state: BoggleStateEnum::Starting,
-            timer: 1000,
-            timer_cancel_token,
+            timer,
             tx,
         }));
 
         let boggle_clone = Arc::clone(&boggle);
+
         tokio::spawn(async move {
             Boggle::start_game_loop(boggle_clone).await;
         });
@@ -66,10 +67,7 @@ impl Boggle {
         match self.state {
             BoggleStateEnum::Starting => Render::starting_state(),
             BoggleStateEnum::InProgress => {
-                let minutes = *&self.timer / 60;
-                let seconds = *&self.timer % 60;
-
-                let fmt_timer = format!("{}:{:02}", minutes, seconds);
+                let fmt_timer = Timer::format_time(Boggle::GAME_DURATION);
                 Render::inprogress_state(&fmt_timer, &self.board, Some(found_words))
             }
             BoggleStateEnum::GameOver => Render::gameover_state(&self.board, &self.players),
@@ -82,15 +80,13 @@ impl Boggle {
             _ => {
                 self.players.remove_inactive();
                 self.players.clear_state();
-                self.start_timer();
+                self.timer.lock().await.start().await;
 
                 self.state = BoggleStateEnum::InProgress;
                 self.board = Board::new(&self.dictionary);
 
-                let minutes = *&self.timer / 60;
-                let seconds = *&self.timer % 60;
+                let fmt_timer = Timer::format_time(Boggle::GAME_DURATION);
 
-                let fmt_timer = format!("{}:{:02}", minutes, seconds);
                 let inprogress_html = Render::inprogress_state(&fmt_timer, &self.board, None);
                 self.broadcast_state(inprogress_html);
             }
@@ -134,7 +130,7 @@ impl Boggle {
         match self.state {
             BoggleStateEnum::Starting => (),
             BoggleStateEnum::InProgress => {
-                self.cancel_timer();
+                self.timer.lock().await.cancel();
                 self.state = BoggleStateEnum::Starting;
             }
             BoggleStateEnum::GameOver => {
@@ -185,54 +181,9 @@ impl Boggle {
         }
     }
 
-    fn start_timer(&self) {
-        let timer_tx = self.tx.clone();
-        let cancel_token = Arc::clone(&self.timer_cancel_token);
-        let boggle_channel_tx = self.boggle_channel_tx.clone();
-
-        // Start at 180 seconds (3 minutes)
-        let timer = Arc::new(Mutex::new(self.timer));
-        tokio::spawn(async move {
-            loop {
-                tokio::select! {
-                    _ = tokio::time::sleep(Duration::from_secs(1)) => {
-                        let mut timer_guard = timer.lock().await;
-
-                        if *timer_guard == 0 {
-                            if let Err(e) = boggle_channel_tx.send(BoggleStateEnum::GameOver) {
-                                eprintln!("Failed to send game over message: {}", e);
-                            }
-                            break;
-                        }
-
-                         *timer_guard -= 1; // Decrement the timer
-
-                        // Convert the remaining time to minutes and seconds
-                        let minutes = *timer_guard / 60;
-                        let seconds = *timer_guard % 60;
-
-                        let fmt_timer = format!("{}:{:02}", minutes, seconds);
-                        let timer_html = Render::timer(&fmt_timer);
-
-                        if let Err(e) = timer_tx.send(timer_html) {
-                            eprintln!("Failed to send timer update: {}", e);
-                        }
-                    },
-                    _ = cancel_token.notified() => {
-                        break;
-                    }
-                }
-            }
-        });
-    }
-
     fn broadcast_state(&self, html: String) {
         if let Err(e) = self.tx.send(html) {
             eprintln!("Failed to broadcast game state: {}", e);
         }
-    }
-
-    fn cancel_timer(&self) {
-        self.timer_cancel_token.notify_one();
     }
 }
