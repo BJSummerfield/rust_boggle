c7248d01ac7ecde287c28285101fd180c7ebaef7
Brian <brian@MacBook-Pro-2.local>
2024-01-28 17:28:44 -0500

    add rate limit and remove unused deps

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use axum::{
+    error_handling::HandleErrorLayer,
+    http::StatusCode,
     routing::{get, post},
-    Extension, Router,
+    BoxError, Extension, Router,
 };
 use std::{env, net::SocketAddr, sync::Arc};
-use time::Duration;
+use tower::{buffer::BufferLayer, limit::RateLimitLayer, ServiceBuilder};
 use tower_http::services::ServeDir;
 use tower_sessions::{Expiry, MemoryStore, SessionManagerLayer};
 
@@ -21,8 +23,7 @@ async fn main() {
     let session_store = MemoryStore::default();
     let session_layer = SessionManagerLayer::new(session_store)
         .with_secure(false)
-        .with_expiry(Expiry::OnInactivity(Duration::seconds(1000)));
-    // .with_expiry(Expiry::OnSessionEnd);
+        .with_expiry(Expiry::OnSessionEnd);
 
     let boggle = Boggle::new();
     let styles_path = env::var("STATIC_FILES_PATH").unwrap_or_else(|_| "/app/static".to_string());
@@ -36,7 +37,19 @@ async fn main() {
         .nest_service("/static", ServeDir::new(styles_path))
         .route("/ws", get(Handle::websocket))
         .with_state(boggle)
-        .layer(session_layer);
+        .layer(session_layer)
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|err: BoxError| async move {
+                    (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Unhandled error: {}", err),
+                    )
+                }))
+                .layer(BufferLayer::new(1024))
+                .layer(RateLimitLayer::new(5, std::time::Duration::from_secs(1))) // Rate limit: 5 requests per second
+                .into_inner(),
+        );
 
     // Bind to a socket address
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
